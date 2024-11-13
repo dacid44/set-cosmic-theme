af634e49c4635550bc7ecf80bc7e4149f8658d68
Keegan Conlee <dacid44@gmail.com>
2024-11-12 19:34:11 -0600

    fix: remove flake dependency on flake-utils

--- flake.nix
+++ flake.nix
@@ -2,12 +2,21 @@
   description = "A small utility to programmatically set a COSMIC Desktop theme from a file";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let pkgs = nixpkgs.legacyPackages.${system}; in
-  {
-    packages.default = import ./default.nix { pkgs = pkgs; };
-  });
+  outputs = { self, nixpkgs }:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    packages = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        default = import ./default.nix { pkgs = pkgs; };
+      }
+    );
+  };
 }
